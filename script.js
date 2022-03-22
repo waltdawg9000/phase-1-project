7f4aab31ac81dcf79228277eb9250238cd8bccdf
Walter Morris <waltercmorris18@gmail.com>
2022-03-22 00:32:10 -0700

    adding grey background

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
+
+document.addEventListener("click", function(event) {
+    //adding the button operational
+    if (!event.target.matches("#button")) return;
+    console.log("Button was Clicked!")
+});
