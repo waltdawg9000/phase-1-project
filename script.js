b9afc99f6967a8532a07eaf1664dad544f9dfd7b
Walter Morris <waltercmorris18@gmail.com>
2022-03-23 16:25:48 -0700

    adding api with key

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-
-document.querySelector("body").appendChild(h2);
-
+let weather = {
+    "apiKey": "0c2d8c723f6a4d209025be1e527fc955",
+    fetchWeather: function() {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=gilroy&units=metric&appid=0c2d8c723f6a4d209025be1e527fc955"
+        ).then((response) => response.json())
+         .then((data) => console.log(data));
+    },
+};
