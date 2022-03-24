a499b92dacdfd66bd791c7ff8a92074675d59e58
Walter Morris <waltercmorris18@gmail.com>
2022-03-23 17:41:48 -0700

    added all the data into a function

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 let weather = {
     "apiKey": "0c2d8c723f6a4d209025be1e527fc955",
-    fetchWeather: function() {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=gilroy&units=metric&appid=0c2d8c723f6a4d209025be1e527fc955"
-        ).then((response) => response.json())
-         .then((data) => console.log(data));
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&units=metric&appid=" 
+        + this.apiKey
+        )
+        
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
     },
+    displayWeather: function(data){
+        const { name } = data;
+        const { description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%"
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/h "
+    }
 };
